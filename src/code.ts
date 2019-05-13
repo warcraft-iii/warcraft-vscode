66c6b09e39aecdbd6b8f64c97037536156e123ab
Dencer <tdaddon@163.com>
2019-05-13 20:37:35 +0800

    chore: wip

--- src/code.ts
+++ src/code.ts
@@ -7,6 +7,9 @@
 
 import * as path from "path";
 import * as fs from "fs-extra";
+import { template, templateSettings } from "lodash";
+
+templateSettings.interpolate = /\-\-\[\[%\=([\s\S]+?)%\]\]/g;
 
 import env from "./environment";
 
@@ -22,44 +25,89 @@ export async function compileDebug(from: string, to: string) {
 
     await fs.mkdirp(path.dirname(to));
 
-    const output = fs.createWriteStream(to, { encoding: "utf-8" });
-    const files = (await getAllFiles(from))
-        .filter(file => !path.basename(file).startsWith(".") && !path.basename(file).startsWith("@"))
-        .filter(file => path.extname(file).toLowerCase() === LUA);
+    const code = (await Promise.all(
+        (await getAllFiles(from))
+            .filter(file => !path.basename(file).startsWith(".") && !path.basename(file).startsWith("@"))
+            .filter(file => path.extname(file).toLowerCase() === LUA)
+            .map(async file => {
+                const body = await fs.readFile(file, { encoding: "utf-8" });
+                const comment = (() => {
+                    const m = body.match(/\[(=*)\[/g);
+                    const exists = new Set(m ? m.map(x => x.length - 2) : []);
 
-    const pipe = (file: string) => {
-        return new Promise((resolve, reject) => {
-            fs.createReadStream(file, {
-                encoding: "utf-8"
+                    let length = 0;
+                    while (exists.has(length)) {
+                        length++;
+                    }
+                    return "=".repeat(length);
+                })();
+
+                const name = path
+                    .relative(from, file)
+                    .replace(LUA_REG, "")
+                    .replace(/[\\\/]+/g, ".");
+
+                return `_PRELOADED['${name}'] = [${comment}[${body}]${comment}]`;
             })
-                .on("close", (err: any) => (err ? reject(err) : resolve()))
-                .pipe(
-                    output,
-                    { end: false }
-                );
-        });
-    };
-    const write = (chunk: string) => {
-        return new Promise((resolve, reject) => {
-            output.write(chunk, (err: any) => (err ? reject(err) : resolve()));
-        });
-    };
+    )).join("\n\n");
 
-    await pipe(path.join(env.extensionFolder, "resources/debug.lua"));
+    const war3map = await fs.readFile(path.join(env.mapFolder, "war3map.lua"), { encoding: "utf-8" });
 
-    for (const file of files) {
-        const name = path
-            .relative(from, file)
-            .replace(LUA_REG, "")
-            .replace(/[\\\/]+/g, ".");
+    const out = template(
+        await fs.readFile(path.join(env.extensionFolder, "templates/debug.lua"), { encoding: "utf-8" })
+    )({
+        code,
+        war3map
+    });
 
-        await write(`_PRELOADED['${name}']=[==========[`);
-        await pipe(file);
-        await write("]==========]");
-        await write("\n");
-    }
-
-    await write("require('war3map')");
-
-    output.close();
+    await fs.writeFile(to, out);
 }
+
+// export async function compileDebug(from: string, to: string) {
+//     if (!(await fs.stat(from)).isDirectory()) {
+//         throw new Error("Not found source folder");
+//     }
+
+//     await fs.mkdirp(path.dirname(to));
+
+//     const output = fs.createWriteStream(to, { encoding: "utf-8" });
+//     const files = (await getAllFiles(from))
+//         .filter(file => !path.basename(file).startsWith(".") && !path.basename(file).startsWith("@"))
+//         .filter(file => path.extname(file).toLowerCase() === LUA);
+
+//     const pipe = (file: string) => {
+//         return new Promise((resolve, reject) => {
+//             fs.createReadStream(file, {
+//                 encoding: "utf-8"
+//             })
+//                 .on("close", (err: any) => (err ? reject(err) : resolve()))
+//                 .pipe(
+//                     output,
+//                     { end: false }
+//                 );
+//         });
+//     };
+//     const write = (chunk: string) => {
+//         return new Promise((resolve, reject) => {
+//             output.write(chunk, (err: any) => (err ? reject(err) : resolve()));
+//         });
+//     };
+
+//     await pipe(path.join(env.extensionFolder, "resources/debug.lua"));
+
+//     for (const file of files) {
+//         const name = path
+//             .relative(from, file)
+//             .replace(LUA_REG, "")
+//             .replace(/[\\\/]+/g, ".");
+
+//         await write(`_PRELOADED['${name}']=[==========[`);
+//         await pipe(file);
+//         await write("]==========]");
+//         await write("\n");
+//     }
+
+//     await write("require('war3map')");
+
+//     output.close();
+// }
---
+++ templates/debug.lua
@@ -0,0 +1,56 @@
+--[[%= war3map %]]
+
+do
+
+local _PRELOADED = {}
+
+--[[%= code %]]
+
+local _LOADED = {}
+
+local function findfile(module)
+    if _PRELOADED[module] then
+        return _PRELOADED[module]
+    end
+
+    module = module .. '.init'
+    if _PRELOADED[module] then
+        return _PRELOADED[module]
+    end
+
+    error('not found ' .. module, 4)
+end
+
+local function loadfile(k)
+    local code = findfile(k)
+    local f, err = load(code, '@' .. k:gsub('%.', '/') .. '.lua')
+    if not f then
+        print(err)
+        return false
+    end
+
+    local ok, r = pcall(f)
+    if not ok then
+        print(r)
+        return false
+    end
+    return r or true
+end
+
+setmetatable(_LOADED, { __index = function(t, k)
+    t[k] = loadfile(k)
+    return t[k]
+end })
+
+function require(module)
+    return _LOADED[module]
+end
+
+end
+
+local orig_main = main
+function main()
+    orig_main()
+    require('lib')
+    require('main')
+end

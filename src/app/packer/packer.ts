cbff0e65cd32ced9b6ab20740191de89cdcdadc2
Jai <814683@qq.com>
2020-07-06 14:29:21 +0800

    chore: Use new MopaqPack.exe

--- src/app/packer/packer.ts
+++ src/app/packer/packer.ts
@@ -30,11 +30,11 @@ class Packer {
         const libs = await fs.readdir(env.asSourcePath(globals.FOLDER_LIBRARIES));
         const imports = [
             globals.FOLDER_IMPORTS,
-            `${globals.FOLDER_IMPORTS}.${ConfigurationType[this.type()].toLowerCase()}`
+            `${globals.FOLDER_IMPORTS}.${ConfigurationType[this.type()].toLowerCase()}`,
         ];
 
         // map.w3x
-        packList.push(...(await this.generatePackItems(env.mapFolder, file => !utils.isLuaFile(file))));
+        packList.push(...(await this.generatePackItems(env.mapFolder, (file) => !utils.isLuaFile(file))));
 
         // imports
         for (const imp of imports) {
@@ -56,18 +56,15 @@ class Packer {
         await fs.writeFile(
             env.asBuildPath(globals.FILE_PACKLIST),
             JSON.stringify(
-                packList.reduceRight(
-                    (r, item) => {
-                        if (!exists.has(item[0])) {
-                            exists.add(item[0]);
-                            r.push(item);
-                        } else {
-                            console.log(item[0], item[1]);
-                        }
-                        return r;
-                    },
-                    [] as PackItem[]
-                )
+                packList.reduceRight((r, item) => {
+                    if (!exists.has(item[0])) {
+                        exists.add(item[0]);
+                        r.push(item);
+                    } else {
+                        console.log(item[0], item[1]);
+                    }
+                    return r;
+                }, [] as PackItem[])
             )
         );
     }
@@ -75,11 +72,15 @@ class Packer {
     async packByPackList() {
         const args: string[] = [];
 
+        args.push('generate');
+
         if (this.type() === ConfigurationType.Debug) {
-            args.push('-f', '1');
+            args.push('-f');
         }
 
-        args.push('-o', env.outFilePath, env.asBuildPath(globals.FILE_PACKLIST));
+        args.push('-o', env.outFilePath);
+
+        args.push('-i', env.asBuildPath(globals.FILE_PACKLIST));
 
         await utils.execFile(env.asExetensionPath('bin/MopaqPack.exe'), args);
     }
@@ -94,11 +95,11 @@ class Packer {
         if (!(await fs.pathExists(root)) || !(await fs.stat(root)).isDirectory()) {
             return [];
         }
-        let files = (await utils.getAllFiles(root)).filter(file => !utils.isHiddenFile(file));
+        let files = (await utils.getAllFiles(root)).filter((file) => !utils.isHiddenFile(file));
         if (filter) {
             files = files.filter(filter);
         }
-        return files.map(file => this.generatePackItem(file, root));
+        return files.map((file) => this.generatePackItem(file, root));
     }
 
     private generatePackItem(file: string, root: string): PackItem {

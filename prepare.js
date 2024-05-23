48109666e2e5b618ea88fbf1a4779893c577f9d4
Jai <814683@qq.com>
2024-05-23 13:21:26 +0800

    fix: prepare.js copy file failed

--- prepare.js
+++ prepare.js
@@ -66,7 +66,7 @@ async function main() {
         }
     }
 
-    await fs.mkdir('out');
+    await fs.mkdirp('out');
     await fs.copyFile("node_modules/wasmoon/dist/glue.wasm", path.join('out', "glue.wasm"));
     await fs.copyFile("node_modules/wasmoon/dist/index.js", path.join('out', "wasmoon.js"));
 }
